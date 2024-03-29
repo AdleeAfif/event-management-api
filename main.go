132976e6459ecc67daee0f425dee7b693ebb8d46
AdleeAfif <kamitokazuya@gmail.com>
2024-03-29 14:18:04 +0800

    Add Get and Post request for event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"project/event-management-api/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +11,34 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/", getDefault)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getDefault(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Hello World"}) //(200, "Hello World")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"event": event})
 }
--- models/event.go
+++ models/event.go
@@ -3,16 +3,20 @@ package models
 import "time"
 
 type Event struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	DateTime    time.Time `json:"datetime"`
-	UserId      int       `json:"user_id"`
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserId      int
 }
 
 var events = []Event{}
 
 func (e Event) Save() {
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
