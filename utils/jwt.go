693f9b8ab54ae198eae6de90ec794df96cfabc77
AdleeAfif <kamitokazuya@gmail.com>
2024-04-02 21:41:34 +0800

    Added JWT token validation

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +18,36 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func ValidateToken(token string) error {
+	token = strings.Split(token, " ")[1]
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	// if !ok {
+	// 	return errors.New("invalid token claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
